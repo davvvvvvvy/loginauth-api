07940fcae38edace7683e9cf966e2427272a1f29
Ivan <46201857+ivekivek@users.noreply.github.com>
2020-11-16 12:25:17 +0100

    Add files via upload

--- server/routes/logreg.ts
+++ server/routes/logreg.ts
@@ -1,6 +1,8 @@
 import * as express from 'express'
 import * as mysql from 'mysql'
+import * as bcrypt from 'bcrypt'
 import {makeId} from '../module/makeId'
+import {selectAll, selectSpecific, insert} from '../module/data'
 
 let router = express.Router()
 let con = mysql.createConnection({
@@ -28,7 +30,9 @@ router.get('/user', (req, res) => {
                     console.log('empty')
                     res.json({message: 'Empty database.'})
                 } else {
-                    res.json({message: '/user', email: respond[0].email, password: respond[0].pass})
+                    res.json({message: '/user',
+                    email: Buffer.from(respond[0].emailPass, 'base64').toString('ascii').split('||')[0],
+                    password: Buffer.from(respond[0].emailPass, 'base64').toString('ascii').split('||')[1]})
                 }
             })
         } catch(e) { console.log(e) }
@@ -48,21 +52,28 @@ router.get('/auth-login', (req, res) => {
         res.redirect(`/user?id=${req.session.name}`)
     } else {
         let token = req.query.token
+        let email = req.headers.email
+        let pass = req.headers.password
+
+        let emailPass = Buffer.from(email+'||'+pass).toString('base64')
 
         try {
-            con.query(`SELECT * FROM users_data WHERE token="${token}";`, (err, respond) => {
-                if (err) console.log(err)
-                if (respond.length <= 0) {
-                    console.log('empty')
-                    res.json({message: 'Empty database.'})
-                } else {
-                    req.session.loggedIn = true
-                    req.session.name = respond[0].id
-                    res.redirect(`/user?id=${req.session.name}`)
-                }
-            })
+            if (bcrypt.compareSync(emailPass, token) == true) {
+                con.query(`SELECT * FROM users_data WHERE token="${token}";`, (err, respond) => {
+                    if (err) console.log(err)
+                    if (respond.length <= 0) {
+                        console.log('empty')
+                        res.json({message: 'Empty database.'})
+                    } else {
+                        req.session.loggedIn = true
+                        req.session.name = respond[0].id
+                        res.redirect(`/user?id=${req.session.name}`)
+                    }
+                })
+            } else {
+                res.json({message: 'Wrong auth.'})
+            }
         } catch(e) { console.log(e) }
-
     }
 })
 
@@ -84,12 +95,16 @@ router.get('/auth-register', (req, res) => {
         //console.log(pass)
 
         let id = makeId()
-        let token = Buffer.from(email+'.'+id+pass).toString('base64')
+        let emailPass = Buffer.from(email+'||'+pass).toString('base64')
 
         try {
-            con.query(`INSERT INTO users_data VALUES ("${id}", "${email}", "${pass}", "${token}");`, (err, respond) => {
-                if (err) console.log(err)
-                res.redirect(`/login`)
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(emailPass, salt, (err, hash) => {
+                    con.query(`INSERT INTO users_data VALUES ( "${id}", "${emailPass}", "${hash}" );`, (err, respond) => {
+                        if (err) console.log(err)
+                        res.redirect('/login')
+                    })
+                })
             })
         } catch(e) { console.log(e) }
     }
