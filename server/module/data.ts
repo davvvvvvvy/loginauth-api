362ce1493abd6800f7477cc25625b546e8d7e779
Ivan <46201857+ivekivek@users.noreply.github.com>
2020-11-16 12:25:40 +0100

    Add files via upload

--- server/module/data.ts
+++ server/module/data.ts
@@ -1,101 +1,49 @@
 import * as mysql from 'mysql'
 
-const DATABASE: String = 'login_api'
+let con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'ivananna',
+    database: 'login_api'
+})
 
-export class Data {
-    
-    public con
-
-    constructor() {
-        this.con = mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: 'ivananna',
-            database: DATABASE
+export function selectAll() {
+    return new Promise(resolve => {
+        con.query('SELECT * FROM users_data;', (err, res) => {
+            if (err) console.log(err)
+            else{ resolve(res) }
         })
-    }
+        con.end()
+    }).catch((e) => { console.log(e) })
+}
 
-    insertData(id: Number, email: String, pass: String, token: String) {
-        this.con.query(`INSERT INTO users_data VALUES (${id}, "${email}", "${pass}", "${token}");`, (err, res) => {
+export function selectSpecific(data: String) {
+    return new Promise(resolve => {
+        con.query(`SELECT * FROM users_data WHERE emailPass = ${data};`, (err, res) => {
+            if (err) console.log(err)
+            if (res.length <= 0) { console.log('error') }
+            else{ resolve(res) }
+        })
+        con.end()
+    }).catch((e) => { console.log(e) })
+}
+
+export function selectToken(token: String) {
+    return new Promise(resolve => {
+        con.query(`SELECT * FROM users_data WHERE token = ${token};`, (err, res) => {
+            if (err) console.log(err)
+            if (res.length <= 0) { console.log('error') }
+            else{ resolve(res) }
+        })
+        con.end()
+    }).catch((e) => { console.log(e) })
+}
+
+export function insert(id: String, emailPass: String, token: String) {
+    return new Promise(resolve => {
+        con.query(`INSERT INTO users_data VALUES ( "${id}", "${emailPass}", "${token}" );`, (err, res) => {
             if (err) console.log(err)
         })
-    }
-    
-    getData() {
-        this.con.query('SELECT * FROM users_data;', (err, res) => {
-            if (err) return err
-            if (res.length <= 0) {
-                return {message: 'empty'}
-            }
-            else {
-                try {
-                    let array = new Array()
-                    for (let i=0; i<res.length; i++) {
-                        array.push({
-                            id: res[i].id,
-                            email: res[i].email,
-                            pass: res[i].pass,
-                            token: res[i].token
-                        })
-                    }
-
-                    return array
-                } catch (e) {
-                    console.log(e)
-                }
-            }
-        })
-    }
-
-    getDataById(id: Number) {
-        this.con.query(`SELECT * FROM users_data WHERE id=${id};`, (err, res) => {
-            if (err) err
-            if (res.length <= 0) {
-                {message: 'empty'}
-            }
-            else {
-                try {
-                    let array = new Array()
-                    array.push({
-                        id: res[0].id,
-                        email: res[0].email,
-                        pass: res[0].pass,
-                        token: res[0].token
-                    })
-
-                    array
-                } catch (e) {
-                    console.log(e)
-                }
-            }
-        })
-    }
-
-    getDataByEmail(email: String) {
-        this.con.query(`SELECT * FROM users_data WHERE id=${email};`, (err, res) => {
-            if (err) return err
-            if (res.length <= 0) {
-                return {message: 'empty'}
-            }
-            else {
-                try {
-                    let array = new Array()
-                    array.push({
-                        id: res[0].id,
-                        email: res[0].email,
-                        pass: res[0].pass,
-                        token: res[0].token
-                    })
-
-                    return array
-                } catch (e) {
-                    console.log(e)
-                }
-            }
-        })
-    }
-
-    close() {
-        this.con.end()
-    }
+        con.end()
+    }).catch((e) => { console.log(e) })
 }
